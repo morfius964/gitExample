19d4b4048efda8edfdde16c8ac2cfceb8b38c8ca
morfius964 <morfius964@hotmail.com>
2016-10-20 16:22:12 -0400

    new file

---
+++ Git/2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  2ViewController.h
+//  Git
+//
+//  Created by MCS on 10/20/16.
+//  Copyright © 2016 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _ViewController : UIViewController
+
+@end
---
+++ Git/2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  2ViewController.m
+//  Git
+//
+//  Created by MCS on 10/20/16.
+//  Copyright © 2016 MCS. All rights reserved.
+//
+
+#import "2ViewController.h"
+
+@interface _ViewController ()
+
+@end
+
+@implementation _ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
