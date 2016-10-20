f02d974b0227737a99a24171d140a7ef201d00b8
morfius964 <morfius964@hotmail.com>
2016-10-20 16:39:57 -0400

    cambios

--- Git/ViewController.m
+++ Git/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do a≤≤≤ny additional setup after loading the view, typically from a nib.
 }
 
 
@@ -28,8 +28,7 @@
     //que ondas nuvo elemeto
     //,jl
     
-    
-    
+    //sdfsdfd
     
 }
 
